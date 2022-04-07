f6ec0094f5074a94579e69537d9ce140d4c82ab0
Tswain <isaactom55@gmail.com>
2022-04-07 11:29:34 -0700

    Being Genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unisd.h>
 
 /**
  * main - Entry point
@@ -8,6 +8,6 @@
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
-return (1);
+	write(2, ch, sizeof(ch));
+	return (1);
 }
